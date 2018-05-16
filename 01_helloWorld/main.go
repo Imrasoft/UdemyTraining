46696132b056b265bf9bc31b0d00a31b58ad6410
imrasoft <mutumba.sowed@gmail.com>
2018-05-16 17:46:44 +0300

    added more lines of code

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("This is my first Go App")
+	fmt.Println("42")
+	fmt.Printf("\n Decimal is %d \n Binary is %b \n Hexadecimal is %x", 42,42,42)
+
+	for i:=60; i<120; i++ {
+		fmt.Printf("\n %d \t %b \t %x \t %q \t %s \n", i,i,i,i,i)
+	}
 }
